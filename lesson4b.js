b23ba719033b4946fd759cb495aea8ebfae2e495
lanshee-world <keyafidel@gmail.com.>
2026-03-03 10:47:52 +0300

    md

--- lesson4b.js
+++ lesson4b.js
@@ -13,3 +13,28 @@ greet();
 })();
 
 // read on arrow functions with and without parameters and research on modules in javascript.
+// arrow functions without parameters
+const greeting = () => console.log("Hello!");
+greeting()
+
+// with a single parameter
+// parenthesis are optional
+const square = x => x * x;
+// or 
+const double = (x) => x * 2;
+
+// with multiple parameters
+const add = (a, b) => a + b;
+
+// JAVASCRIPT MODULES
+
+// Export:used to make variables,functions or classes available to other files 
+// Named exports - for multiple values (export const myVar = 10;).
+// Default export - for one primary value per module(export default myFunction;)
+
+// Import:use to bring functionality from other modules.
+// example - Import {myVar} from './module.js';
+
+// usage in browser: to use native ES modules in a browser, you must include the type="module" attribute in your <script> tag.
+// HTML
+// <script type="module" src="main.js"></script>
